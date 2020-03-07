8284c98326abf9a25a400b87e5b97f6fe288c61c
anujjain5699 <56468658+anujjain5699@users.noreply.github.com>
2020-03-07 14:35:02 +0530

    Add files via upload
    java code to download youtube videos in just one click

---
+++ youtubedownloader.java
@@ -0,0 +1,19 @@
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Scanner;
+public class youtubedownloader {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("enter URl");
+        StringBuffer url = new StringBuffer(sc.next());
+        url.insert(12, "ss");
+         Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.browse(URI.create(url.toString()));
+        } catch ( IOException e)
+        {
+            e.printStackTrace();
+        } 
+    }
+}
